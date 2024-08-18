a31aa5571fe2244a7f2d64705620c00913cb09ff
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-18 12:26:09 -0500

    service terminado

--- src/app/services/libro.service.ts
+++ src/app/services/libro.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Api } from '../api';
+
+const url = Api.API + "/libro";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibroService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  consultaLibros(titulo : string, anio : number, estado : number, idcategoria : number) : Observable<any> {
+    let parametros = new HttpParams().set("titulo", titulo).set("anio", anio).set("estado", estado).set("idcategoria", idcategoria);
+    return this.http.get<any>(url + "/consultaLibro", {params: parametros});
+  }
+
 }
