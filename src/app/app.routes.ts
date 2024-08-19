9fec992e48e7bcda79ffa04230722e0a54fa85f1
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-19 18:29:32 -0500

    terminado examen simulacro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ConsultaLibrosComponent } from './components/consulta-libros/consulta-libros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "consultaLibros", component: ConsultaLibrosComponent},
+    {path: "", component: ConsultaLibrosComponent, pathMatch: "full"}
+];
